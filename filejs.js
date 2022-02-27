d83cb152d937b5506de1ff4aab3573c8e0572c8a
DuongOcean <nguyendaiduong3587@gmail.com>
2022-02-27 09:47:44 +0700

    test

--- filejs.js
+++ filejs.js
@@ -1,1 +1,11 @@
+function a()
+{
+    var img = document.getElementById("img");
+    img.src = "Tahmken.png";
+}
+function b()
+{
+    var img = document.getElementById("img");
+    img.src = "anh.jpg";
+}
 
