7a4b45969b46ff7439a78926f6c0827a66ae4d94
DuongOcean <nguyendaiduong3587@gmail.com>
2022-03-02 07:58:44 +0700

    test

--- filejs.js
+++ filejs.js
@@ -1,11 +1,1 @@
-function a()
-{
-    var img = document.getElementById("img");
-    img.src = "Tahmken.png";
-}
-function b()
-{
-    var img = document.getElementById("img");
-    img.src = "anh.jpg";
-}
 
